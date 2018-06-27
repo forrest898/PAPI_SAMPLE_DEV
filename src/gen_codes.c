15c9bbe6678721c510f96197782cc49f3750a4fb
forrest898 <fwsmith898@gmail.com>
2018-06-27 09:46:36 -0400

    sleeping helped... figured out my problem when I read the documentation more closely. needed to declare perf_event_attr before using pfm_get_os_encode with PFM_OS_PERF_EVENT

--- src/gen_codes.c
+++ src/gen_codes.c
@@ -17,17 +17,21 @@
 #include <sys/prctl.h>
 #include <err.h>
 #include <perfmon/pfmlib.h>
+#include <perfmon/perf_event.h>
+#include <perfmon/pfmlib_perf_event.h>
 #include <inttypes.h>
 #include <papi.h>
+
 #include "char_replace.h"
-#include "perf_event.h"
+//#include "perf_event.h"
 #include "test_utils.h"
-#include "perf_helpers.h"
+//#include "perf_helpers.h"
 
 
 int main(int argc, char** argv) {
 
     pfm_perf_encode_arg_t raw;
+    struct perf_event_attr attr;
     int ret, i;
     char *event = "BR_INST_RETIRED.ALL_BRANCHES";
 
@@ -51,22 +55,28 @@ int main(int argc, char** argv) {
     while((read = getline(&line, &len, fp)) != -1) {
 
         memset(&raw, 0, sizeof(raw));
+        raw.size = sizeof(raw);
+        raw.attr = &attr;
 
         //printf("%d\n",read);
         line[(read -1)] = '\0';
         event = line;
 
+        //printf("%s\n", event);
         //printf("\t\t\t//%s\n", event);
-
-        ret = pfm_get_os_event_encoding(event, PFM_PLM3, PERF_OS_PERF_EVENT, &raw);
+        //PERF_OS_PERF_EVENT PERF_OS_PERF_EVENT_EXT PERF_OS_EVENT PFM_OS_PERF_EXT PFM_OS_PERF
+        ret = pfm_get_os_event_encoding(event, PFM_PLM3, PFM_OS_PERF_EVENT, &raw);
         if (ret != PFM_SUCCESS)
   // err(1, " cannot get encoding %s", pfm_strerror(ret));
             printf("gen_codes can't get encoding %s\n",  pfm_strerror(ret));
         else {
             replace_char(line, '.', '_');
             printf("\t\tcase\tPAPI_%s\t:\n", line);
+            /*
             for(i=0; i < raw.count; i++)
                 printf("\t\t\tpe.config=0x%"PRIx64"\n", raw.codes[i]);
+            */
+            printf("\t\t\tpe.config=0x%x\n", raw.attr->config);
             printf("\t\t\tbreak;\n");
         }
 
@@ -75,7 +85,7 @@ int main(int argc, char** argv) {
     fclose(fp);
     if(line)
         free(line);
-    free(raw.codes);
+    //free(raw);
 
     return 0;
 }
